823098cca14bcdf0bc91913601a7b4acab6ef428
juchafernandez <juchafernandez@atanasoff05.danielcastelao.org>
2016-06-20 09:42:33 +0200

    Format hecho

--- src/examenfinalparte2/Principal.java
+++ src/examenfinalparte2/Principal.java
@@ -1,35 +1,38 @@
-
 package examenfinalparte2;
 
 import java.util.Scanner;
+
 /**
- * Comprueba si un número es primo o no y se mostrarán los números primos
- * de el número de cifras que se introduzca 
+ * Comprueba si un número es primo o no y se mostrarán los números primos de el
+ * número de cifras que se introduzca
+ *
  * @author juchafernandez
  */
 
-public class Principal {public static boolean esPrimo = false;
+public class Principal {
+
+    public static boolean esPrimo = false;
 
     public static void main(String arg[]) {
-       int dig;
+        int dig;
         int ndig;
-       do {
-           dig = introducirDigito();
-        if (dig <= 0) {
-        System.out.println("Ingrese como parámetro, un numero de digito correcto (mayor que 0): ");
+        do {
+            dig = introducirDigito();
+            if (dig <= 0) {
+                System.out.println("Ingrese como parámetro, un numero de digito correcto (mayor que 0): ");
             }
-        } while (dig <= 0); 
-       for (int numComparar = 1; numComparar <= 99999; numComparar++) {
-            ndig = contarDivisionEntera(numComparar); 
-            
-            
-     if (ndig == dig) {
-                if (numComparar < 4) { 
+        } while (dig <= 0);
+        for (int numComparar = 1; numComparar <= 99999; numComparar++) {
+            ndig = contarDivisionEntera(numComparar);
+
+            if (ndig == dig) {
+                if (numComparar < 4) {
                     esPrimo = true;
                 } else if (numComparar % 2 == 0) {
-          esPrimo = false;
-                } else { 
-                    int countDivisores = 0;        int i1 = 1;
+                    esPrimo = false;
+                } else {
+                    int countDivisores = 0;
+                    int i1 = 1;
                     int limite = (numComparar - 1) / 2;
                     if (limite % 2 == 0) {
                         limite--;
@@ -43,8 +46,10 @@ public class Principal {public static boolean esPrimo = false;
             }
         }
     }
+
     /**
      * Calcula el número de divisores del número pasado
+     *
      * @param i1 Divisor
      * @param limite Límite de divisores
      * @param numComparar Número del que se van a contar los divisores
@@ -62,20 +67,22 @@ public class Principal {public static boolean esPrimo = false;
                 i1 = limite + 1;
             }
         }
-        
+
         if (countDivisores == 1) {
             esPrimo = true;
         }
-        
+
         return esPrimo;
     }
+
     /**
      * Cuenta el número de divisiones enteras que se pueden realizar sobre el
      * número pasado por parámetros.
+     *
      * @param numComparar Número dividendo
      * @return El número de divisiones enteras
      */
-private static int contarDivisionEntera(int numComparar) {
+    private static int contarDivisionEntera(int numComparar) {
         int aux = numComparar;
         int contador = 0;
         while (aux != 0) {
@@ -84,8 +91,10 @@ private static int contarDivisionEntera(int numComparar) {
         }
         return contador;
     }
-/**
+
+    /**
      * Pide un número entero por pantalla
+     *
      * @return El número entero introducido por el usuario
      */
     public static int introducirDigito() {
